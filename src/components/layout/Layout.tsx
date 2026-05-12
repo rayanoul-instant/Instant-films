dfe05a7f8da40866c659dea2c20be39fb31889df
rayanoul-instant <rayanoul.987@icloud.com>
2026-05-12 02:14:02 +0200

    Hide footer on court métrage detail and message thread pages

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -8,18 +8,25 @@ interface LayoutProps {
   showNavLogo?: boolean;
 }
 
+const HIDE_FOOTER_PATHS = ['/courts-metrages/'];
+
 export function Layout({ children }: LayoutProps) {
   const location = useLocation();
+
+  const hideFooter =
+    HIDE_FOOTER_PATHS.some(p => location.pathname.startsWith(p)) ||
+    (location.pathname === '/messages' && location.search.includes('with='));
 
   return (
     <div className="min-h-screen flex flex-col">
       <main className={`flex-1 pb-6 md:pb-0 ${location.pathname !== '/' ? 'md:pt-20' : ''}`}>
         {children}
       </main>
-      {/* pb-28 on mobile so footer clears the fixed BottomNav */}
-      <div className="pb-28 md:pb-0">
-        <Footer />
-      </div>
+      {!hideFooter && (
+        <div className="pb-28 md:pb-0">
+          <Footer />
+        </div>
+      )}
     </div>
   );
 }
